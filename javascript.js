95f6c2d7ad259f3acf00f2a0ebb02809b19fe7ff
undeesr <tejassinghknp@gmail.com>
2022-12-29 13:57:26 +0530

    Added hover effect in the pixels

--- javascript.js
+++ javascript.js
@@ -1,13 +1,34 @@
 const sketchPad = document.querySelector("#sketchpad");
 
 function createGrid() {
+  const pixels = [];
+
   for (let i = 0; i < 16 * 16; i++) {
     const gridBlock = document.createElement("div");
     gridBlock.classList.add("pixel");
     sketchPad.appendChild(gridBlock);
+
+    pixels.push(gridBlock);
   }
+
+  return pixels;
 }
 
-createGrid();
+function hoverEffect(pixel) {
 
-console.log(sketchPad);
+    const originalColor = pixel.style.backgroundColor;
+
+  pixel.addEventListener("pointerenter", () => {
+    pixel.style.backgroundColor = "black";
+  });
+
+  pixel.addEventListener("pointerleave", () => {
+    pixel.style.backgroundColor = originalColor;
+  })
+}
+
+const pixels = createGrid();
+
+pixels.forEach(hoverEffect);
+
+console.log(sketchPad, pixels);
