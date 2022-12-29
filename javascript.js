334b18f6a259ffd9565d585aafec02bf54d936ee
undeesr <tejassinghknp@gmail.com>
2022-12-29 13:41:02 +0530

    Created a 16x16 grid (doesn't appear yet)

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const sketchPad = document.querySelector("#sketchpad");
+
+function createGrid() {
+  for (let i = 0; i < 16 * 16; i++) {
+    const gridBlock = document.createElement("div");
+    gridBlock.classList.add("pixel");
+    sketchPad.appendChild(gridBlock);
+  }
+}
+
+console.log(sketchPad);
